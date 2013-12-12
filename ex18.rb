0331df6cbec4e4af3f7c093fc0bb41d92b43f1d5
William Mathewson <wncmathewson@me.com>
2013-12-12 12:29:38 +0000

    Commiting to GitHub

---
+++ ex18.rb
@@ -0,0 +1,22 @@
+# This one is like your scripts with argv
+def puts_two(*args)
+	arg1, arg2 = args
+	puts "arg1: #{arg1}, arg2: #{arg2}"
+end
+
+def puts_two_again(arg1, arg2)
+	puts "arg1: #{arg1}, arg2: #{arg2}"
+end
+
+def puts_one(arg1)
+	puts "arg1: #{arg1}"
+end
+
+def puts_none
+	puts "Sorry, out of stock!"
+end
+
+puts_two("Will", "Mathewson")
+puts_two_again("Will", "Mathewson")
+puts_one("First!")
+puts_none()
---
+++ ex32.rb
@@ -0,0 +1,26 @@
+the_count = [1, 2, 3, 4, 5]
+fruits = ["apples", "oranges", "pears", "apricots"]
+change = [1, "pennies", 2, "dimes", 3, "quarters"]
+
+# This kind of for-loop goes through an array
+for number in the_count
+	puts "This is count #{number}."
+end
+
+# Same as above, but using a block instead
+fruits.each do |fruit|
+	puts "A fruit of type: #{fruit}"
+end
+
+# Also we can go through mixed arrays too
+for i in change
+	puts "I have #{i}"
+end
+
+# This should work. Works like elements = range(6) in Python
+elements = (0..5).to_a
+
+# Now we can puts them out too
+for i in elements
+	puts "Element was: #{i}"
+end
